7f8cbfa5a55c63a81a653b5b392e79c1b9519371
Yaroslav <43764315+fullstackdev90@users.noreply.github.com>
2020-09-17 13:07:26 +0300

    test1
    1.0 test

---
+++ elevator.php
@@ -0,0 +1,24 @@
+Class elevator{
+    Public move{ 
+      If (isset($people== true){
+          if(isset(move==true) {moveDown||lightOn}
+          elseif( isset (move==false){moveDown|| lightOn})
+      elseif(move==false){
+      lightOff; Exit; 
+      }
+   Private function lightOn(){
+   // 
+   } 
+   Private function lightOff(){ 
+   // 
+   } 
+   Private function moveDown(){ 
+   // 
+   } 
+   Private function moveUp(){ 
+   // 
+   }  
+  Public $color; 
+  Public $material;  
+  } 
+} 
