8b96a0f5dce9994fb5c1adc98a01ed189f805f5c
Yaroslav <43764315+fullstackdev90@users.noreply.github.com>
2020-09-17 16:52:42 +0300

    Update elevator.php

--- elevator.php
+++ elevator.php
@@ -1,24 +1,39 @@
+<?php
 Class elevator{
-    Public move{ 
-      If (isset($people== true){
-          if(isset(move==true) {moveDown||lightOn}
-          elseif( isset (move==false){moveDown|| lightOn})
-      elseif(move==false){
-      lightOff; Exit; 
+    Public function $move{ 
+      If($people== true){
+          if(move==true) {$this->moveDown()||$this->lightOn()}
+          elseif($this->move()==false){$this->moveUp()||$this->lightOn()})
+      elseif($this->move()==false){
+      lightOff; 
+          Exit; 
       }
-   Private function lightOn(){
+   Private $lightOn = 1;
+   Private function $lightOff=0;
+   Private function $moveDown(){ 
    // 
    } 
-   Private function lightOff(){ 
-   // 
-   } 
-   Private function moveDown(){ 
-   // 
-   } 
-   Private function moveUp(){ 
+   Private function $moveUp(){ 
    // 
    }  
   Public $color; 
   Public $material;  
   } 
-} 
+}
+    $passengers = new elevator;
+$passengers->setColor="grey";
+$passengers->setLight="TRUE";
+$passengers->setStop="TRUE";
+$passengers->setMaterial="glass and metal";
+$passengers->setSpeed="2km";
+
+$cargo =new elevator;
+$cargo->setCarrying="8";
+$cargo->setColor="grey";
+$cargo->setLight="TRUE";
+$cargo->setStop="TRUE";
+$cargo->setMaterial="metal";
+$cargo->setSpeed="1km";
+$a=[$cargo];
+print_r($a);
+?>
